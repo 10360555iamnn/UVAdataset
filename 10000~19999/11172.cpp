c71e5886eef4eb1a4e52a5970470cba79311547f
10360555iamnn <95529963+10360555iamnn@users.noreply.github.com>
2024-04-24 02:57:53 +0800

    Create 11172.cpp

---
+++ 10000~19999/11172.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int a,b,t;cin>>t;
+  while(t--){
+    cin>>a>>b;
+    if(a==b)cout<<"=\n";
+    else if(a>b)cout<<">\n";
+    else cout<<"<\n";
+  }
+}
